b0abe5576dfa8516e0e5acfc6118d0315f47a31f
tomkf <thomaskeithferris@gmail.com>
2020-02-23 19:01:34 -0800

    step list view

--- app/src/main/java/com/tomkf/cookit/RecipeDetailActivity.kt
+++ app/src/main/java/com/tomkf/cookit/RecipeDetailActivity.kt
@@ -3,18 +3,25 @@ package com.tomkf.cookit
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_recipe_detail.*
+import kotlinx.android.synthetic.main.recipe_step_list.view.*
 
 class RecipeDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_detail)
-
-        // get the recipe from the intent, if null will throw NPE
         val recipe: Recipe = intent?.extras?.getParcelable(recipeExtra)!!
-
         recipe_name.text = recipe.name
+        Glide.with(this).load(recipe.imageURL).into(recipe_image)
+        recipe_steps.layoutManager = LinearLayoutManager(this)
+        recipe_steps.adapter = StepsAdapter(recipe.steps, this)
     }
 
     companion object {
@@ -27,3 +34,30 @@ class RecipeDetailActivity : AppCompatActivity() {
         }
     }
 }
+
+private class StepsAdapter(
+    val steps: List<String>,
+    val context: Context
+) :
+    RecyclerView.Adapter<StepsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StepsViewHolder {
+        return StepsViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.recipe_step_list,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return steps.count()
+    }
+
+    override fun onBindViewHolder(holder: StepsViewHolder, position: Int) {
+        val step = steps[position]
+        holder.itemView.recipe_step.text = step
+    }
+}
+
+private class StepsViewHolder(view: View) : RecyclerView.ViewHolder(view)
